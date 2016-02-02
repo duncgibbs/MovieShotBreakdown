1897fbc1cee8cac0d893e4d71ef10a0191785f9e
Duncan Gibbs <duncgibbs@gmail.com>
2016-02-02 14:27:37 -0600

    now compares color and greyscale histograms; comments out debugs; calculates average take length

--- MovieShotFinder.py
+++ MovieShotFinder.py
@@ -16,8 +16,11 @@ proc = sp.Popen(command,stdout=sp.PIPE, stderr=sp.PIPE)
 proc.stdout.readline()
 proc.terminate()
 infos = proc.stderr.read()
+
 (x,y) = re.search('\s\d+x\d+\s',infos).group(0).split('x')
 resolution = [int(x), int(y)]
+
+videoFPS = float(re.search('\S+\sfps',infos).group(0).split(' ')[0])
 
 command = [FFMPEG_BIN, '-i', fileName,'-f','image2pipe','-pix_fmt','rgb24','-vcodec','rawvideo','-']
 pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
@@ -26,8 +29,12 @@ frame = 1
 shot = 1
 differences = []
 differenceMean = 0
-dirname = 'Shot_1'
-os.mkdir(dirname)
+
+shots = []
+
+#DEBUG
+#dirname = 'Shot_1'
+#os.mkdir(dirname)
 
 raw_image = pipe.stdout.read(resolution[0]*resolution[1]*3)
 imageOne = np.fromstring(raw_image, dtype='uint8')
@@ -36,51 +43,62 @@ pipe.stdout.flush()
 
 while True:
 
+	if len(differences) == 100:
+		differences = differences[1:]
+
 	frame += 1
 
 	raw_image = pipe.stdout.read(resolution[0]*resolution[1]*3)
 
 	if not raw_image:
+		shots += [(frame-1)/videoFPS]
 		break;
 
 	imageTwo = np.fromstring(raw_image, dtype='uint8')
 	imageTwo = imageTwo.reshape((resolution[1],resolution[0],3))
 	pipe.stdout.flush()
 
-	greyOne = cv2.cvtColor(imageOne, cv2.COLOR_BGR2RGB)
-	histOne = cv2.calcHist([greyOne],[0,1,2],None,[16,16,16],[0,256,0,256,0,256])
-	histOne = cv2.normalize(histOne).flatten()
+	recoloredOne = cv2.cvtColor(imageOne, cv2.COLOR_BGR2RGB)
+	greyOne 	 = cv2.cvtColor(imageOne, cv2.COLOR_BGR2GRAY)
+	colorHistOne = cv2.calcHist([recoloredOne],[0,1,2],None,[16,16,16],[0,256,0,256,0,256])
+	colorHistOne = cv2.normalize(colorHistOne).flatten()
+	greyHistOne  = cv2.calcHist([greyOne],[0],None,[256],[0,256])
+	greyHistOne  = cv2.normalize(greyHistOne)
 
-	greyTwo = cv2.cvtColor(imageTwo, cv2.COLOR_BGR2RGB)
-	histTwo = cv2.calcHist([greyTwo],[0,1,2],None,[16,16,16],[0,256,0,256,0,256])
-	histTwo = cv2.normalize(histTwo).flatten()
+	recoloredTwo = cv2.cvtColor(imageTwo, cv2.COLOR_BGR2RGB)
+	greyTwo 	 = cv2.cvtColor(imageTwo, cv2.COLOR_BGR2GRAY)
+	colorHistTwo = cv2.calcHist([recoloredTwo],[0,1,2],None,[16,16,16],[0,256,0,256,0,256])
+	colorHistTwo = cv2.normalize(colorHistTwo).flatten()
+	greyHistTwo  = cv2.calcHist([greyTwo],[0],None,[256],[0,256])
+	greyHistTwo  = cv2.normalize(greyHistTwo)
 
-	#differenceRating = dist.chebyshev(histOne,histTwo)
-	differenceRating = dist.euclidean(histOne,histTwo)**2
+	colorDifferenceRating = dist.euclidean(colorHistOne,colorHistTwo)
+	greyDifferenceRating  = dist.euclidean(greyHistOne,greyHistTwo)**2
+	differenceRating 	  = (colorDifferenceRating*greyDifferenceRating)
 
-	if differenceRating > differenceMean*30 and differenceMean > 0:
-		differences = []
+
+	if differenceRating > differenceMean*100 and differenceMean > 0 and differenceRating > (differences[len(differences)-1]*80) and differenceRating > .001:
+		#DEBUG
+		#print str((frame-1)/videoFPS) + ' seconds (' + str(frame-1) + ' frames at ' + str(videoFPS) + ' frames per second)'
+
+		differences    = []
 		differenceMean = 0
-		shot += 1
-		dirname = 'Shot_' + str(shot);
-		os.mkdir(dirname)
+		shot 		   += 1
+		shots 		   += [(frame-1)/videoFPS]
+		frame 		   = 0
+
+		#DEBUG
+		#dirname = 'Shot_' + str(shot);
+		#os.mkdir(dirname)
 	else:
 		differences += [differenceRating]
 		differenceMean = sum(differences)/len(differences)
 
-	# if len(differences) > 1 and (differenceRating > (sum(differences)/len(differences))*7 and (sum(differences)/len(differences)) > 0):
-	# 	differences = []
-	# 	shot += 1
-	# 	dirname = 'Shot_' + str(shot);
-	# 	os.mkdir(dirname)
-	# else:
-	# 	differences += [differenceRating]
-
-	print 'FR: ' + str(frame) + ' --- ' + 'DR: ' + str(differenceRating*100) + ' --- ' + 'MEAN: ' + str(differenceMean*100)
-
-	cv2.imwrite(os.path.join(dirname,'Frame_' + str(frame) + ' - ' + str(differenceRating*100) + '.png'),greyTwo)
-	#cv2.imwrite('Frame_' + str(frame) + ' - ' + str(differenceRating*100) + '.png',greyTwo)
+	#DEBUG
+	#print 'FR: ' + str(frame) + ' --- ' + 'DR: ' + str(differenceRating*100) + ' --- ' + 'MEAN: ' + str(differenceMean*100)
+	#cv2.imwrite(os.path.join(dirname,'Frame_' + str(frame) + ' - ' + str(differenceRating) + '.png'),recoloredTwo)
 
 	imageOne = imageTwo
 
 pipe.terminate()
+print '# OF SHOTS: ' + str(len(shots)) + ' - AVG. TAKE LENGTH: ' + str("{0:.2f}".format(sum(shots)/len(shots))) + ' seconds'
